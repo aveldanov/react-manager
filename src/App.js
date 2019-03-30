d75c7cda429f18c5c9dc5ba044d0eac7b22690a7
Anton Veldanov <anton.veldanov@gmail.com>
2019-03-29 19:12:31 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,19 +3,30 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 33 },
+      { name: 'John', age: 40 },
+      { name: 'Steph', age: 70 }
+    ]
+  }
 
+  switchNameHandler = () => {
+    console.log('Was clicked');
+
+  }
 
   render() {
     return (
       <div className="App">
         <h1>Hello</h1>
         <p>It is working</p>
-        <button>Switch Name</button>
-        <Person name="max" age="90" />
-        <Person name="max1" age="90">I have a hobby</Person>
-        <Person name="max2" age="902" />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I have a hobby</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
 
-      </div>
+      </div >
     );
   }
 }
