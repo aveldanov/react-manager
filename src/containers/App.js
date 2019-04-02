06d43d1aec2f317a1230fd2099f18a4961776483
Anton Veldanov <anton.veldanov@gmail.com>
2019-04-01 19:42:34 -0700

    hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,6 +6,23 @@ import Cockpit from '../components/Cockpit/Cockpit';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js getDerivedStateFromProps', props);
+    return state;
+
+  }
+
+  componentDidMount() {
+    console.log('[App.js componentDidMount');
+
+  }
+
+
   state = {
     persons: [
       { id: '1', name: 'Max', age: 33 },
@@ -60,6 +77,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js render');
 
     let persons = null;
     if (this.state.showPersons) {
